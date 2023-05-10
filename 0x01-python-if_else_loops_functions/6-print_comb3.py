e3b71da9a3a54011e7f381d04faf0ce8bea73f67
Austasty <obuesiogonna2@gmail.com>
2023-05-09 18:05:04 -0700

    The two digits must be different

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(9):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
             continue
-        print("{}{}".format(i, j, end=", "))
+        print("{}{}".format(i, j), end=", ")
