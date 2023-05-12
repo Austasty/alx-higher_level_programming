e393afcc1ceb892fa2792946c3066d0abd4f928a
Austasty <obuesiogonna2@gmail.com>
2023-05-11 23:23:11 -0700

    importing multiple modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,16 @@
-from calculator_1.py import add, substract, multiply and divide
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
 total = add(a, b)
-substraction = substract(a, b)
-multiplcation = multiply(a, b)
-division = divide(a, b)
+substraction = sub(a, b)
+multiplication = mul(a, b)
+division = div(a, b)
 
 print(f"{a} + {b} = {total}")
-print(f"{} - {} = {substraction}")
-print(f"{} * {} = {multiplication}")
-print(f"{} / {} = {division}")
+print(f"{a} - {b} = {substraction}")
+print(f"{a} * {b} = {multiplication}")
+print(f"{a} / {b} = {division}")
