2bbf7c011afa463fbcacf7b6f3ccff5e5b446c7c
Austasty <obuesiogonna2@gmail.com>
2023-08-07 16:03:20 -0700

    edited

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ last = word[-2:]
 middle = word[1:-1]
 print("First 3 letters: {}".format(first))
 print("Last 2 letters: {}".format(last))
-print("Middle word: {}".format(word))
+print("Middle word: {}".format(middle))
