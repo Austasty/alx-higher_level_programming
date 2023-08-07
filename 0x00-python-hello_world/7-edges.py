6a5d4a99f82ced833f70a3bf984aa582cacadf12
Austasty <obuesiogonna2@gmail.com>
2023-08-07 16:01:26 -0700

    python scripyts

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[:3]
+last = word[-2:]
+middle = word[1:-1]
+print("First 3 letters: {}".format(first))
+print("Last 2 letters: {}".format(last))
+print("Middle word: {}".format(word))
