704c23b15f82c3032faec06b702c6f9a01bf6925
Austasty <obuesiogonna2@gmail.com>
2023-08-08 14:04:18 -0700

    insomnia game

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""Printing of the alphabets in lowercase and not followed by a new line."""
+for alpha in range(97, 123):
+    print("{}".format(chr(alpha), end="")
