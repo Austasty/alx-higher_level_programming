1a0558327502c1de55636fcbaac9814b1006313f
Austasty <obuesiogonna2@gmail.com>
2023-08-08 20:22:08 -0700

    updated 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for dig in range(0, 10):
     for digit in range(dig + 1, 10):
         if dig == 8 and digit == 9:
-            print("{}{}".format(digit1, digit2))
+            print("{}{}".format(dig, digit))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(dig, digit), end=", ")
