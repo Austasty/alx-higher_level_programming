d6b6a836bc882d8a0ab8a83926177bd820c12386
Austasty <obuesiogonna2@gmail.com>
2023-09-02 00:02:05 -0700

    added all

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Square class."""
+
 
 class Square:
+    """ square instance"""
 
     def __init__(self, size=0):
+        """ new square.
 
+        Args:
+            size (int): size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -11,4 +18,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """current area of the square."""
         return (self.__size * self.__size)
