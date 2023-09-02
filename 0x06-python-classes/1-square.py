feaa861f5981acb49d1e9787fdd81ab5ab4d5adc
Austasty <obuesiogonna2@gmail.com>
2023-09-01 23:51:17 -0700

    added all

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+""" A class Square."""
+
+
 class Square:
+    """a square instance"""
+
     def __init__(self, size):
+        """new Square.
+
+        Args:
+            size (int):  new square size.
+        """
         self.__size = size
