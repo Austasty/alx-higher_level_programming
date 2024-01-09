5b3386e00a17e8e3558e5df8e0cb824d0bed708c
Austasty <obuesiogonna2@gmail.com>
2024-01-09 19:39:09 +0100

    added files for solution on scpes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    }
+  }
+};
